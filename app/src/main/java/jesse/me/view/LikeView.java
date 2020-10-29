3088e3edd87da0375462b94fef0daba4d8df58de
Jesse <jesse920524@163.com>
2020-10-29 21:55:08 +0800

    文字动画效果进行中

--- app/src/main/java/jesse/me/view/LikeView.java
+++ app/src/main/java/jesse/me/view/LikeView.java
@@ -1,5 +1,7 @@
 package jesse.me.view;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
@@ -15,6 +17,8 @@ import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
 
+import java.util.Collections;
+
 import jesse.me.R;
 
 /**
@@ -28,6 +32,7 @@ public class LikeView extends View {
     private boolean mLike;
 
     private Paint mTextPaint;
+    private Paint mTextPaint1;
     private Paint mBitmapPaint;
 
     private Bitmap mBmpNormal;
@@ -35,7 +40,11 @@ public class LikeView extends View {
     private Bitmap mBmpDecor;
     private int mLikeCnt;
 
-    private float scaleFact;
+    private float scaleFact;//缩放系数
+    private float transYUp;//上移动画系数
+    private float transYDown;//下移动画系数
+    private float alphaIn;//渐显动画系数
+    private float alphaOut;//渐隐动画系数
 
     public LikeView(Context context) {
         super(context);
@@ -66,14 +75,53 @@ public class LikeView extends View {
         invalidate();
     }
 
+    public float getTransYUp() {
+        return transYUp;
+    }
+
+    public void setTransYUp(float transYUp) {
+        this.transYUp = transYUp;
+        invalidate();
+    }
+
+    public float getTransYDown() {
+        return transYDown;
+    }
+
+    public void setTransYDown(float transYDown) {
+        this.transYDown = transYDown;
+        invalidate();
+    }
+
+    public float getAlphaIn() {
+        return alphaIn;
+    }
+
+    public void setAlphaIn(float alphaIn) {
+        this.alphaIn = alphaIn;
+        invalidate();
+    }
+
+    public float getAlphaOut() {
+        return alphaOut;
+    }
+
+    public void setAlphaOut(float alphaOut) {
+        this.alphaOut = alphaOut;
+        invalidate();
+    }
+
     private void init(){
         scaleFact = 1.0f;
         mLike = false;
         mLikeCnt = 1000;
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setColor(Color.parseColor("#CECECE"));
         mTextPaint.setTextSize(SystemUtil.sp2px(getContext(), 16));
+        mTextPaint1.setColor(Color.parseColor("#CECECE"));
+        mTextPaint1.setTextSize(SystemUtil.sp2px(getContext(), 16));
     }
 
     public boolean isLike() {
@@ -86,17 +134,59 @@ public class LikeView extends View {
     }
 
     public void click(){
+        setLike(!mLike);
 
+        if (mLike){
+            mLikeCnt++;
+        }else{
+            mLikeCnt--;
+        }
+
+        /**对icon应用scaleIn -> scaleOut 动画*/
         ObjectAnimator scaleIn = ObjectAnimator.ofFloat(this, "scaleFact", 1f, 0.9f);
         ObjectAnimator scaleOut = ObjectAnimator.ofFloat(this, "scaleFact", 0.9f, 1.0f);
 
+        scaleIn.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+//                setLike(!mLike);
+            }
+        });
+
+        /**icon 动画*/
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playSequentially(scaleIn, scaleOut);
-        animatorSet.setDuration(240);
+        animatorSet.setDuration(100);
         animatorSet.setInterpolator(new LinearInterpolator());
         animatorSet.start();
-//        invalidate();
-        this.mLike = !mLike;
+
+        /**对文字最后一位应用translationY && alpha动画, 将文字最后一位上弹&&渐隐
+         *
+         * 对文字最后一位应用translationY && alpha动画, 将新的最后一位上弹&&渐显
+         *
+         * */
+
+        ObjectAnimator transYUp = ObjectAnimator.ofFloat(this, "transYUp", 0, 1);
+        ObjectAnimator transYDown = ObjectAnimator.ofFloat(this, "transYDown", 0, 1);
+        ObjectAnimator alphaIn = ObjectAnimator.ofFloat(this, "alphaIn", 0, 1);
+        ObjectAnimator alphaOut = ObjectAnimator.ofFloat(this, "alphaOut", 1, 0);
+
+        AnimatorSet textAnimSet = new AnimatorSet();
+        if (mLike){
+            textAnimSet.playTogether(alphaOut, transYUp);
+        }else{
+            textAnimSet.playTogether(alphaOut, transYDown);
+        }
+
+        textAnimSet.setDuration(200).start();
+        // TODO: 2020/10/27 1. 文字需要对个位进行动画, 要得到文字个位数的值
+
+        // TODO: 2020/10/27 translationY需要移动文字高度的距离, 需测量文字高度: getTextBounds()
+
+        // TODO: 2020/10/27 translationY与alpha需同时执行
+
+
     }
 
     @Override
@@ -110,20 +200,28 @@ public class LikeView extends View {
     }
 
     private void drawTexts(Canvas canvas) {
-        String text = String.valueOf(mLikeCnt);
+        String text = String.valueOf(mLikeCnt-1);
+        String text1 = String.valueOf(mLikeCnt);
 
         Rect bounds = new Rect();
         mTextPaint.getTextBounds(text, 0, text.length()-1, bounds);
         int x = mBmpNormal.getWidth() + SystemUtil.dp2px(getContext(), 5);
         int y = getHeight()/2 - (mBmpNormal.getHeight()/2 - bounds.height()/2);
-        canvas.drawText(String.valueOf(mLikeCnt),  x, y, mTextPaint);
+        canvas.drawText(text,  x, y-(bounds.height()*transYUp), mTextPaint);
+
+        mTextPaint.setAlpha((int) (255*alphaOut));
+        canvas.drawText(text1, x, y-(bounds.height()*transYUp) + bounds.height(), mTextPaint1);
+
     }
 
     private void drawDecor(Canvas canvas) {
-        if (mLike == false) return;
+
         mBmpDecor = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_message_like_shining);
+        canvas.save();
+        canvas.scale(scaleFact, scaleFact, mBmpDecor.getWidth()/2, mBmpDecor.getHeight()/2);
         canvas.drawBitmap(mBmpDecor, 0+SystemUtil.dp2px(getContext(), 2),
                 getHeight()/2- mBmpLike.getHeight() -mBmpDecor.getHeight()/2, mBitmapPaint);
+        canvas.restore();
     }
 
     private void drawBmp(Canvas canvas) {
@@ -157,7 +255,7 @@ public class LikeView extends View {
                 getHeight()/2 - mBmpLike.getHeight(), mBitmapPaint);
         canvas.restore();
 
-//        drawDecor(canvas);
+        drawDecor(canvas);
     }
 
 
